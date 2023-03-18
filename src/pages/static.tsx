00412fbaa8760b65c4314baae1db7069040b2095
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2023-03-18 20:52:00 -0300

    feat: get static component props

--- src/pages/static.tsx
+++ src/pages/static.tsx
@@ -1,9 +1,29 @@
 import Static_one from "@/components/static_one"
 
-export default function Static() {
+export function getStaticProps() {
+    return {
+        props: {
+            number: Math.floor(Math.random() * 10) + 1
+        },
+        revalidate: 30
+    }
+}
+
+
+export default function Static(props: any) {
     return (
         <>
-          <Static_one/>
+          <div className="static">
+            <Static_one/>
+          </div>
+          <div className="static">
+            <h1>
+                Static #02
+            </h1>
+            <h2>
+                Random number integer: {props.number}
+            </h2>
+          </div>
         </>
     )
 }
